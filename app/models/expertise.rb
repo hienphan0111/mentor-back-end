fc631f0d18fd1acace68825213213ecad4f3e604
hienphan0111 <hienphan@digitalcad.edu.vn>
2023-06-16 15:42:37 +0700

    Add reservation and expertise model

--- app/models/expertise.rb
+++ app/models/expertise.rb
@@ -1,2 +1,6 @@
 class Expertise < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 20, maximum: 128 }
+  validates :description, length: { maximum: 500 }
+
+  belongs_to :mentor
 end
