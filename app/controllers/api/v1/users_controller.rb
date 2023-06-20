8af9620c29fb570d072c45f43a89aa25952a4750
hienphan0111 <hienphan@digitalcad.edu.vn>
2023-06-20 20:39:55 +0700

    Fix lint err

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class UsersController < Api::V1::ApplicationController
-      skip_before_action :doorkeeper_authorize!, only: %i[create]
+      skip_before_action :doorkeeper_authorize!, only: [:create]
 
       def create
         user = User.new(
@@ -16,7 +16,7 @@ module Api
         return render(json: { error: 'Invalid client ID' }, status: 403) unless client_app
 
         if user.save
-          create_token(user)
+          create_token(user, client_app)
         else
           render(json: { error: user.errors.full_messages, data: params }, status: 422)
         end
@@ -24,7 +24,7 @@ module Api
 
       private
 
-      def create_token(user)
+      def create_token(user, client_app)
         access_token = Doorkeeper::AccessToken.create(
           resource_owner_id: user.id,
           application_id: client_app.id,
@@ -34,16 +34,16 @@ module Api
         )
 
         render(json: {
-                      user:  {
-                        id: user.id,
-                        email: user.email,
-                        access_token: access_token.token,
-                        token_type: 'bearer',
-                        expires_in: access_token.expires_in,
-                        refresh_token: access_token.refresh_token,
-                        created_at: access_token.created_at.to_time.to_i
-                      }
-                     })
+          user: {
+            id: user.id,
+            email: user.email,
+            access_token: access_token.token,
+            token_type: 'bearer',
+            expires_in: access_token.expires_in,
+            refresh_token: access_token.refresh_token,
+            created_at: access_token.created_at.to_time.to_i
+          }
+        })
       end
 
       def user_params
@@ -52,7 +52,7 @@ module Api
 
       def generate_refresh_token
         loop do
-          # generate a random token string and return it,
+          # Generate a random token string and return it,
           # unless there is already another token with the same string
           token = SecureRandom.hex(32)
           break token unless Doorkeeper::AccessToken.exists?(refresh_token: token)
