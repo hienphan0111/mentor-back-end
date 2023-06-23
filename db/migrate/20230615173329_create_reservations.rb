4dea3d67d851adedb0c63fbc6f9ba2ffb30e6d0e
Chris <hakizachrist1@gmail.com>
2023-06-23 13:45:18 +0200

    Update tables

--- db/migrate/20230615173329_create_reservations.rb
+++ db/migrate/20230615173329_create_reservations.rb
@@ -1,7 +1,7 @@
 class CreateReservations < ActiveRecord::Migration[7.0]
   def change
     create_table :reservations do |t|
-      t.time :time
+      t.datetime :time
       t.text :message
       t.references :mentor, null: false, foreign_key: { to_table: :mentors }, index: true
       t.references :user, null: false, foreign_key: { to_table: :users }, index: true
